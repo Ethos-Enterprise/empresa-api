15d24f18587e49df61e5359958eed1a99b68a0a1
Jefferson <jefferson.silva@jazztech.com.br>
2023-09-22 12:48:06 -0300

    adding aws connection

--- data/ddl.sql
+++ data/ddl.sql
@@ -1,9 +1,22 @@
 create TABLE IF NOT EXISTS EMPRESA  (
   id uuid PRIMARY KEY,
-  razaoSocial varchar(300) NOT NULL,
+  razaoSocial varchar(300) NOT NULL UNIQUE,
   cnpj varchar(14) NOT NULL UNIQUE,
-  telefone varchar(14) NOT NULL UNIQUE,
+  telefone varchar(14) UNIQUE,
   email varchar(300) NOT NULL UNIQUE,
   senha varchar(300) NOT NULL,
-  qtdFuncionarios int NOT NULL
+  setor varchar(300),
+  qtdFuncionarios int
+);
+
+create TABLE IF NOT EXISTS ENDERECO  (
+  id UUID PRIMARY KEY,
+  numero varchar(5),
+  cep varchar(10),
+  logradouro varchar(200),
+  complemento varchar(300),
+  bairro varchar(300),
+  uf char(2),
+  fk_empresa UUID,
+  foreign key (fk_empresa) references empresa(id)
 );
--- src/main/java/com/ethos/empresaapi/repository/entity/EnderecoEntity.java
+++ src/main/java/com/ethos/empresaapi/repository/entity/EnderecoEntity.java
@@ -24,7 +24,6 @@ public class EnderecoEntity {
     String bairro;
 
     String uf;
-
     public EnderecoEntity() {
     }
     @Builder(toBuilder = true)
