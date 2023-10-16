867ba84d16b63bd43fcd22645be9cbc326493007
Jefferson <jefferson.silva@jazztech.com.br>
2023-10-16 12:10:36 -0300

    adding validation telefone validation and qtd funcionarios validation

--- src/main/java/com/ethos/empresaapi/EmpresaApiApplication.java
+++ src/main/java/com/ethos/empresaapi/EmpresaApiApplication.java
@@ -2,10 +2,12 @@ package com.ethos.empresaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class EmpresaApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/ethos/empresaapi/controller/request/EmpresaRequest.java
+++ src/main/java/com/ethos/empresaapi/controller/request/EmpresaRequest.java
@@ -1,16 +1,19 @@
 package com.ethos.empresaapi.controller.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import org.hibernate.validator.constraints.br.CNPJ;
 
 public record EmpresaRequest(@NotBlank(message = "razão social em branco") @NotNull(message = "razão social nula") String razaoSocial,
                              @NotBlank(message = "cnpj em branco") @NotNull(message = "cnpj nulo") @CNPJ(message = "Cnpj inválido") String cnpj,
-                             @NotBlank(message = "telefone em branco") @NotNull(message = "telefone nulo") String telefone,
+                             @NotBlank(message = "telefone em branco") @NotNull(message = "telefone nulo") @Size(min = 11, max = 12, message = "Quantidade de caracteres no telefone inválido") String telefone,
                              @NotNull(message = "email nulo") @NotBlank(message = "email em branco") @Email(message = "email inválido") String email,
                              @NotNull(message = "senha nula") @NotBlank(message = "senha em branco") String senha, String setor,
+                             @Min(value = 0, message = "Quantidade de funcionários deve ser um número positivo")
                              Integer qtdFuncionarios,
                              EnderecoRequest endereco) {
     public record EnderecoRequest(String cep, String numero, String complemento) {
