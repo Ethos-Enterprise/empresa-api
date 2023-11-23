db2adc22adf736378c71519bbcc4ab541fe942c1
Jefferson <jefferson.silva@jazztech.com.br>
2023-11-23 19:47:05 -0300

    fix| plano

--- src/main/java/com/ethos/empresaapi/repository/entity/EmpresaEntity.java
+++ src/main/java/com/ethos/empresaapi/repository/entity/EmpresaEntity.java
@@ -1,12 +1,13 @@
 package com.ethos.empresaapi.repository.entity;
 
-import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import org.hibernate.annotations.Formula;
-import org.hibernate.annotations.Immutable;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.UUID;
+import lombok.Builder;
 
 @Table(name = "EMPRESA")
 @Entity
@@ -39,8 +40,8 @@ public class EmpresaEntity {
     }
 
     @Builder(toBuilder = true)
-    public EmpresaEntity(String razaoSocial, String cnpj, String telefone, String email, String senha, String setor,
-                         Integer qtdFuncionarios, Boolean assinanteNewsletter, EnderecoEntity endereco, int plano) {
+    public EmpresaEntity(String razaoSocial, String cnpj, String telefone, String email, String senha, String setor, Integer qtdFuncionarios,
+                         Boolean assinanteNewsletter, EnderecoEntity endereco, int plano) {
         this.id = UUID.randomUUID();
         this.razaoSocial = razaoSocial;
         this.cnpj = cnpj;
@@ -55,82 +56,84 @@ public class EmpresaEntity {
     }
 
 
-    public EmpresaEntity(UUID id, String razaoSocial, String cnpj, String telefone, String email, int quantidadeFuncionarios, String setor, Boolean assinanteNewsletter, int endereco, int plano) {
+    public EmpresaEntity(UUID id, String razaoSocial, String cnpj, String telefone, String email, int quantidadeFuncionarios, String setor,
+                         Boolean assinanteNewsletter, int endereco, int plano) {
     }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public void setRazaoSocial(String razaoSocial) {
-        this.razaoSocial = razaoSocial;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public void setSetor(String setor) {
-        this.setor = setor;
-    }
-
-    public void setQtdFuncionarios(Integer qtdFuncionarios) {
-        this.qtdFuncionarios = qtdFuncionarios;
-    }
     public void setAssinanteNewsletter(Boolean assinanteNewsletter) {
         this.assinanteNewsletter = assinanteNewsletter;
-    }
-
-    public void setEndereco(EnderecoEntity endereco) {
-        this.endereco = endereco;
     }
 
     public UUID getId() {
         return id;
     }
 
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
     public String getRazaoSocial() {
         return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
     }
 
     public String getCnpj() {
         return cnpj;
     }
 
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
     public String getTelefone() {
         return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getSenha() {
         return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 
     public String getSetor() {
         return setor;
     }
 
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
     public EnderecoEntity getEndereco() {
         return endereco;
     }
 
+    public void setEndereco(EnderecoEntity endereco) {
+        this.endereco = endereco;
+    }
+
     public Integer getQtdFuncionarios() {
         return qtdFuncionarios;
+    }
+
+    public void setQtdFuncionarios(Integer qtdFuncionarios) {
+        this.qtdFuncionarios = qtdFuncionarios;
     }
 
     public void setPlano(int plano) {
