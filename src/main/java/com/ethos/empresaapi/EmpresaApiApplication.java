5a3e2808de8a4b4e0c8936a41329115c58526209
Jefferson <jefferson.silva@jazztech.com.br>
2023-11-23 19:51:38 -0300

    adicionando teste

--- src/main/java/com/ethos/empresaapi/EmpresaApiApplication.java
+++ src/main/java/com/ethos/empresaapi/EmpresaApiApplication.java
@@ -12,6 +12,7 @@ public class EmpresaApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmpresaApiApplication.class, args);
+        System.out.println("Empresa API running...");
     }
 
 }
