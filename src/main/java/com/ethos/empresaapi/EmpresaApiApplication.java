3a2ed17eb188f761fdcf702d75c4b681dd4728b1
Jefferson-Araujo-Silva <jefferson.silva@sptech.school>
2024-05-27 22:26:20 -0300

    teste com watchower

--- src/main/java/com/ethos/empresaapi/EmpresaApiApplication.java
+++ src/main/java/com/ethos/empresaapi/EmpresaApiApplication.java
@@ -12,7 +12,7 @@ public class EmpresaApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmpresaApiApplication.class, args);
-        System.out.println("Empresa API running...");
+        System.out.println("Teste watchtower");
     }
 
 }
